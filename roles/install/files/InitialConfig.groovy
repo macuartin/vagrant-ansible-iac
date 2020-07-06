8dd2b64be53c50be25b7ae2b037d43a34bb18578
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-07-06 10:50:16 -0500

    :sparkles: Initial Commit

---
+++ roles/install/files/InitialConfig.groovy
@@ -0,0 +1,10 @@
+import jenkins.model.*
+import jenkins.install.InstallState
+Jenkins.instance.setNumExecutors(0)
+
+Jenkins.instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
+
+url = System.env.JENKINS_URL
+urlConfig = JenkinsLocationConfiguration.get()
+urlConfig.setUrl(url)
+urlConfig.save()
