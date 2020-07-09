08c38761c67a25f1c7afdfc17867e1046408fd13
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-07-09 09:42:14 -0500

    change admin account user

---
+++ roles/install/files/InitialConfig.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/env groovy
+
+import jenkins.model.*
+import hudson.security.*
+
+
+def instance = Jenkins.get()
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount('admin','admin')
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+instance.setAuthorizationStrategy(strategy)
+
+instance.save()
